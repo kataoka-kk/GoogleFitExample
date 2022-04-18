38b90739f357cdf70a444d777127bd7cca817e21
kataoka-kk <horeizai.1201@gmail.com>
2022-04-18 12:44:52 +0900

    update

--- app/src/main/java/com/example/googlefitexample/MainActivity.kt
+++ app/src/main/java/com/example/googlefitexample/MainActivity.kt
@@ -41,6 +41,8 @@ class MainActivity : AppCompatActivity() {
                 GoogleSignIn.getLastSignedInAccount(this),
                 fitnessOptions
             )
+        } else {
+            readData()
         }
     }
 
@@ -48,71 +50,75 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
-                // 認証成功時の処理を書く
-                Log.d(tag, "認証成功")
-
-                val TAG = "Fitness"
-                // 取得範囲の指定
-                val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
-                val start = sdf.parse("2022/04/01 00:00:00").time
-                val end = sdf.parse("2022/04/31 23:59:59").time
-
-                val request = DataReadRequest.Builder()
-                    .aggregate(DataType.TYPE_STEP_COUNT_DELTA)
-                    .setTimeRange(start, end, TimeUnit.MILLISECONDS)
-                    // 取得間隔を変えれる（以下なら１日単位）
-                    .bucketByTime(1, TimeUnit.DAYS)
-                    .build()
-
-                // 歩数取得
-                GoogleSignIn.getLastSignedInAccount(this)?.let {
-                    Fitness.getHistoryClient(this, it)
-                        .readData(request)
-                        .addOnSuccessListener {
-                            val buckets = it.buckets
-                            buckets.forEach { bucket ->
-                                val st = bucket.getStartTime(TimeUnit.MILLISECONDS)
-                                val ed = bucket.getEndTime(TimeUnit.MILLISECONDS)
-
-                                Log.d(
-                                    tag,
-                                    "TYPE_STEP_COUNT_DELTA:${bucket.getDataSet(DataType.TYPE_STEP_COUNT_DELTA)}"
-                                )
-
-                                Log.d(
-                                    tag,
-                                    "AGGREGATE_STEP_COUNT_DELTA:${bucket.getDataSet(DataType.AGGREGATE_STEP_COUNT_DELTA)}"
-                                )
-
-                                Log.d(
-                                    tag,
-                                    "TYPE_STEP_COUNT_CUMULATIVE:${bucket.getDataSet(DataType.TYPE_STEP_COUNT_CUMULATIVE)}"
-                                )
-
-                                try {
-                                    val dataSet =
-                                        bucket.getDataSet(DataType.AGGREGATE_STEP_COUNT_DELTA)
-                                    val value =
-                                        dataSet?.dataPoints?.first()?.getValue(Field.FIELD_STEPS)
-                                    Log.d(
-                                        TAG,
-                                        "取得成功 ${sdf.format(Date(st))} ～ ${sdf.format(Date(ed))} ${value}歩"
-                                    )
-                                } catch (ex: Exception) {
-                                    Log.d(TAG, "Exception ${ex.message}")
-                                }
-                            }
-                        }
-                        .addOnFailureListener {
-                            Log.d(TAG, "失敗 ${it.message}")
-                        }
-                        .addOnCanceledListener {
-                            Log.d(TAG, "キャンセル")
-                        }
-                }
+                readData()
             } else {
                 // 認証失敗時の処理を書く
             }
+        }
+    }
+
+    private fun readData() {
+        // 認証成功時の処理を書く
+        Log.d(tag, "認証成功")
+
+        val TAG = "Fitness"
+        // 取得範囲の指定
+        val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+        val start = sdf.parse("2022/04/01 00:00:00").time
+        val end = sdf.parse("2022/04/31 23:59:59").time
+
+        val request = DataReadRequest.Builder()
+            .aggregate(DataType.TYPE_STEP_COUNT_DELTA)
+            .setTimeRange(start, end, TimeUnit.MILLISECONDS)
+            // 取得間隔を変えれる（以下なら１日単位）
+            .bucketByTime(1, TimeUnit.DAYS)
+            .build()
+
+        // 歩数取得
+        GoogleSignIn.getLastSignedInAccount(this)?.let {
+            Fitness.getHistoryClient(this, it)
+                .readData(request)
+                .addOnSuccessListener {
+                    val buckets = it.buckets
+                    buckets.forEach { bucket ->
+                        val st = bucket.getStartTime(TimeUnit.MILLISECONDS)
+                        val ed = bucket.getEndTime(TimeUnit.MILLISECONDS)
+
+                        Log.d(
+                            tag,
+                            "TYPE_STEP_COUNT_DELTA:${bucket.getDataSet(DataType.TYPE_STEP_COUNT_DELTA)}"
+                        )
+
+                        Log.d(
+                            tag,
+                            "AGGREGATE_STEP_COUNT_DELTA:${bucket.getDataSet(DataType.AGGREGATE_STEP_COUNT_DELTA)}"
+                        )
+
+                        Log.d(
+                            tag,
+                            "TYPE_STEP_COUNT_CUMULATIVE:${bucket.getDataSet(DataType.TYPE_STEP_COUNT_CUMULATIVE)}"
+                        )
+
+                        try {
+                            val dataSet =
+                                bucket.getDataSet(DataType.AGGREGATE_STEP_COUNT_DELTA)
+                            val value =
+                                dataSet?.dataPoints?.first()?.getValue(Field.FIELD_STEPS)
+                            Log.d(
+                                TAG,
+                                "取得成功 ${sdf.format(Date(st))} ～ ${sdf.format(Date(ed))} ${value}歩"
+                            )
+                        } catch (ex: Exception) {
+                            Log.d(TAG, "Exception ${ex.message}")
+                        }
+                    }
+                }
+                .addOnFailureListener {
+                    Log.d(TAG, "失敗 ${it.message}")
+                }
+                .addOnCanceledListener {
+                    Log.d(TAG, "キャンセル")
+                }
         }
     }
 }
